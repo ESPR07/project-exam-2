a9511ca5dd1e4c65ccd44e45240f5c5621764100
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2024-05-18 18:44:40 +0200

    added proper validation to the register form

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -3,18 +3,36 @@ import styles from "./RegisterPage.module.css";
 import Button from "../../components/common/Button";
 import { registerAuthEvents } from "../../API/Auth/registerAuthEvent";
 import { redirect } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 const API_BASE = process.env.API_BASE_URL;
 const API_REGISTER_PATH = process.env.API_REGISTER;
 const API_REGISTER_URL = `${API_BASE}${API_REGISTER_PATH}`
 
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+  avatarURL?: string | null;
+  bannerURL?: string | null;
+}
+
+const userSchema = yup.object().shape({
+  name: yup.string().required('Name is required').matches(/^\S+$/, 'No spaces allowed'),
+  email: yup.string().email('Invalid email format').required('Email is required').matches(/^[\w.%+-]+@stud\.noroff\.no$/, 'Email must be a @stud.noroff.no address'),
+  password: yup.string().required('Password is required'),
+  avatarURL: yup.string().url('Invalid URL format').notRequired(),
+  bannerURL: yup.string().url('Invalid URL format').notRequired()
+})
+
 function RegisterPage() {
   const [isManager, setIsManager] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [avatarURL, setAvatarURL] = useState<string>("");
-  const [bannerURL, setBannerURL] = useState<string>("");
+
+  const { register, handleSubmit, formState: { errors }} = useForm<Inputs>({
+    resolver: yupResolver<Inputs>(userSchema),
+  });
 
   const {RegisterFetch} = registerAuthEvents();
 
@@ -26,22 +44,23 @@ function RegisterPage() {
     setIsManager(false);
   }
 
-  async function registerEvent() {
+  async function registerEvent(data: Inputs) {
+    console.log(data);
     const registerDetails = {
       method: "POST",
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
       body: JSON.stringify({
-        name: name,
-        email: email,
-        password: password,
+        name: data.name,
+        email: data.email,
+        password: data.password,
         avatar: {
-          url: avatarURL,
+          url: data.avatarURL,
           alt: "Avatar Image"
         },
         banner: {
-          url: bannerURL,
+          url: data.bannerURL,
           alt: "Banner Image"
         },
         venueManager: isManager
@@ -49,7 +68,7 @@ function RegisterPage() {
     }
     const response = await RegisterFetch(API_REGISTER_URL, registerDetails);
     if(response?.ok !== false) {
-      alert(`Welcome to Holidaze ${name}`);
+      alert(`Welcome to Holidaze ${data.name}`);
       redirect("/");
     }
   }
@@ -57,10 +76,7 @@ function RegisterPage() {
   return(
     <main className={styles.pageContent}>
       <h1>Register Account</h1>
-      <form className={styles.registerForm} onSubmit={(e) => {
-        e.preventDefault();
-        registerEvent();
-      }}>
+      <form className={styles.registerForm} onSubmit={handleSubmit(registerEvent)}>
         <h2>Are you a manager?</h2>
         <div className={styles.tickBoxContainer}>
           <p>Yes</p>
@@ -73,18 +89,23 @@ function RegisterPage() {
         <h2>Who are you?</h2>
         <div className={styles.inputContainer}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" value={name} placeholder="Name Namesson" onChange={(e) => {setName(e.target.value)}} required/>
+          <input type="text" id="name" placeholder="Name Namesson" {...register("name")}/>
+          {errors.name ? <p className={styles.errorText}>{errors.name.message}</p> : ""}
           <label htmlFor="email">Email</label>
-          <input type="email" id="name" value={email} placeholder="@stud.noroff.no" onChange={(e) => {setEmail(e.target.value)}} required/>
+          <input type="email" id="email" placeholder="@stud.noroff.no" {...register("email")}/>
+          {errors.email ? <p className={styles.errorText}>{errors.email.message}</p> : ""}
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="********" value={password} onChange={(e) => {setPassword(e.target.value)}} required/>
+          <input type="password" id="password" placeholder="********" {...register("password")}/>
+          {errors.password ? <p className={styles.errorText}>{errors.password.message}</p> : ""}
         </div>
         <h2>Add a picture!</h2>
         <div className={styles.avatarBanner}>
           <label htmlFor="avatar">Avatar URL</label>
-          <input type="text" id="avatar" value={avatarURL} placeholder="www.imagelocation.com/image.jpg" onChange={(e) => {setAvatarURL(e.target.value)}}/>
+          <input type="text" id="avatar" {...register("avatarURL")}/>
+          {errors.avatarURL ? <p className={styles.errorText}>{errors.avatarURL.message}</p> : ""}
           <label htmlFor="banner">Banner URL</label>
-          <input type="text" id="banner" value={bannerURL} placeholder="www.imagelocation.com/image.jpg" onChange={(e) => {setBannerURL(e.target.value)}}/>
+          <input type="text" id="banner" {...register("bannerURL")}/>
+          {errors.bannerURL ? <p className={styles.errorText}>{errors.bannerURL.message}</p> : ""}
         </div>
         <Button text="Register" type="submit" event={() => {}}/>
       </form>
